1c74568e022da6ed1fd2c0e57689b72a26f47b33
QsVlEANN <evgeniy.radchin@stud.onu.edu.ua>
2022-06-02 22:38:14 +0300

    do not commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,8 @@ public class Main {
         }
     }
 
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ви самі хочете ввести дані? (так/ні): ");
